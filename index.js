98de7100611b71986c0651b1ff9be5fd90b5761e
Anna Revidovich <nutkatuz@yandex.ru>
2020-07-02 17:08:32 +0500

    -

--- index.js
+++ index.js
@@ -10,73 +10,62 @@ const todos = [
 
 const todosListElement = document.querySelector('.todos__list');
 const todosFormElement = document.querySelector('.todos__form');
-const todosSubmitBtn = todosFormElement.querySelector('.todos__submit-btn');
-const todosInputElement = document.querySelector('.todos__input');
-const todosTemplateElement =  document.querySelector('.todo-template');
+const todosInputElement = todosFormElement.querySelector('.todos__input');
+const todosFormSubmitButton = todosFormElement.querySelector('.todos__submit-btn')
+const todoTemplateElement = document.querySelector('.todo-template');
 
-let editedElement = null;
-
-function addTodoListeners(todo) {
-  todo.querySelector('.todo__btn_type_delete').addEventListener('click', handleDelete);
-  todo.querySelector('.todo__btn_type_edit').addEventListener('click', handleEdit);
-  todo.querySelector('.todo__btn_type_duplicate').addEventListener('click', handleDuplicate);
-}
-
-function handleDelete(evt) {
-  const todo = evt.currentTarget.parentNode;
-  todo.remove();
-}
-
-function setTodoToForm(todo) {
-  const text = todo.querySelector('.todo__text').textContent;
-  todosInputElement.value = text;
-  todosSubmitBtn.textContent = 'Сохранить';
-}
-
-function resetTodoForm() {
-  editedElement = null;
-  todosInputElement.value = '';
-  todosSubmitBtn.textContent = 'Добавить';
-}
-
-function handleEdit(evt) {
-  const todo = evt.currentTarget.parentNode;
-  
-  editedElement = todo;
-  setTodoToForm(todo);
-}
-
-function handleDuplicate(evt) {
-  const todo = evt.currentTarget.parentNode;
-  const duplicate = todo.cloneNode(true);
-
-  addTodoListeners(duplicate);
-  todo.after(duplicate);
-}
+let editedTodo = null;
 
 function addTodo(text) {
-  const todo = todosTemplateElement.content.cloneNode(true);
-
+  const todo = todoTemplateElement.content.cloneNode(true);
   todo.querySelector('.todo__text').textContent = text;
   addTodoListeners(todo);
-
   todosListElement.prepend(todo);
 }
 
-function handleFormSubmit (evt) {
-  evt.preventDefault();
+function addTodoListeners(todo) {
+  todo.querySelector('.todo__btn_type_delete').addEventListener('click', handleDeleteTodo);
+  todo.querySelector('.todo__btn_type_edit').addEventListener('click', handleEditTodo);
+  todo.querySelector('.todo__btn_type_duplicate').addEventListener('click', handleDuplicateTodo);
+}
 
+function handleDeleteTodo(e) {
+  const todo = e.target.closest('.todo');
+  todo.remove();
+}
+
+function handleEditTodo(e) {
+  const todo = e.target.closest('.todo');
+  setTodotoForm(todo);
+}
+
+function handleDuplicateTodo(e) {
+  const todo = e.target.closest('.todo');
+  const newTodo = todo.cloneNode(true);
+  addTodoListeners(newTodo);
+  todo.after(newTodo);
+}
+
+function setTodotoForm(todo) {
+  editedTodo = todo;
+  const text = todo.querySelector('.todo__text').textContent;
+  todosInputElement.value = text;
+  todosFormSubmitButton.textContent = 'Сохранить';
+}
+
+function handleTodoFormSubmit(e) {
+  e.preventDefault();
   const text = todosInputElement.value;
-
-  if (editedElement) {
-    editedElement.querySelector('.todo__text').textContent = text;
+  todosInputElement.value = '';
+  if (editedTodo) {
+    editedTodo.querySelector('.todo__text').textContent = text;
+    todosFormSubmitButton.textContent = 'Добавить';
+    editedTodo = null;
   } else {
     addTodo(text);
   }
-
-  resetTodoForm();
 }
 
-todos.forEach(addTodo);
+todos.forEach(text => addTodo(text));
 
-todosFormElement.addEventListener('submit', handleFormSubmit);
+todosFormElement.addEventListener('submit', handleTodoFormSubmit);
